22f66bbefec54b3a281613189df7f740a5c21fec
Lnk-dev <53863852+Lnk-dev@users.noreply.github.com>
2020-02-04 15:21:48 -0500

    Add files via upload

---
+++ src/html5up-multiverse/Home.svelte
@@ -0,0 +1,14 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<iframe src="tile.html"
+style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">
+</iframe>
+
+</main>
+
+<style>
+	
+</style>
